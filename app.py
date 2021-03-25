cd7958cef38cf4d1a15faa6abc2f48cb6d402b2b
Rivaltino Arron <arron2501@gmail.com>
2021-03-26 00:49:28 +0700

    Penambahan komentar pada kode, dan beberapa peningkatan performa

--- app.py
+++ app.py
@@ -2,12 +2,15 @@ from flask import Flask, render_template, redirect, request, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
-from models import formMember
+# Mengimport file model
+from models import formPatient
 
-# pip install flask-WTF
-# pip install flask-sqlalchemy
-# pip install pymysql
+# Plugin flask yang harus terinstall agar aplikasi ini dapat berjalan
+# [1] pip install flask-WTF
+# [2] pip install flask-sqlalchemy
+# [3] pip install pymysql
 
+# Konfigurasi aplikasi
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'y0nnf4n5clu8@#$%^&*'
 app.config['SQLALCHEMY_DATABASE_URI'] = \
@@ -17,78 +20,100 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
 
-class Member(db.Model):
-    __tablename__ = 'members'
+# Inisialisasi database, menggunakan SQLAlchemy
+class Patients(db.Model):
+    __tablename__ = 'patients'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(30), nullable=False)
-    address = db.Column(db.String(200), nullable=True)
-    phone = db.Column(db.String(15), nullable=False)
-    email = db.Column(db.String(30), nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+    address = db.Column(db.String(200), nullable=False)
+    phone = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    complaint = db.Column(db.String(200), nullable=False)
     created_at = db.Column(db.TIMESTAMP, nullable=True)
     updated_at = db.Column(db.TIMESTAMP, nullable=True)
 
-    def __init__(self, id, name, address, phone, email, created_at, updated_at):
+    def __init__(self, id, name, address, phone, email, complaint, created_at, updated_at):
         self.id = id
         self.name = name
         self.address = address
         self.phone = phone
         self.email = email
+        self.complaint = complaint
         self.created_at = created_at
         self.updated_at = updated_at
 
     def __repr__(self):
-        return '[%s, %s, %s, %s, %s, %s, %s]' % \
-               (self.id, self.name, self.address, self.phone, self.email, self.created_at, self.updated_at)
+        return '[%s, %s, %s, %s, %s, %s, %s, %s]' % \
+               (self.id, self.name, self.address, self.phone, self.email, self.complaint, self.created_at, self.updated_at)
 
 
+# Route ke halaman utama, dan mengirimkan data dari database ke html
 @app.route('/')
 def index():
-    form = formMember()
-    return render_template('index.html', form=form, data=Member.query.all())
+    form = formPatient()
+    return render_template('index.html', form=form, data=Patients.query.all())
 
 
-@app.route('/addmember', methods=['GET', 'POST'])
-def addmember():
-    form = formMember()
+# Route untuk menambah pasien baru
+@app.route('/addpatient', methods=['GET', 'POST'])
+def addpatient():
+    form = formPatient()
+    # Apabila methodnya POST, maka aplikasi akan mengirimkan data ke database
     if request.method == 'POST':
         name = form.name.data
         address = form.address.data
         phone = form.phone.data
         email = form.email.data
+        complaint = form.complaint.data
         created_at = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         updated_at = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        member = Member(id, name, address, phone, email, created_at, updated_at)
-        db.session.add(member)
+        patient = Patients(id, name, address, phone, email, complaint, created_at, updated_at)
+        db.session.add(patient)
         db.session.commit()
-        flash("Member added successfully!")
+        # Pesan flash ketika pasien berhasil ditambahkan
+        flash("Patient added successfully!")
+        # Dan akan redirect ke halaman utama
         return redirect(url_for('index'))
+    # Apabila methodnya GET, maka aplikasi akan menampilkan halaman utama
     else:
         return render_template('index.html', form=form)
 
 
-@app.route('/editmember', methods=['GET', 'POST'])
-def editmember():
-    form = formMember()
-    member = Member.query.get(request.form.get('id'))
+# Route untuk mengedit data pasien
+@app.route('/editpatient', methods=['GET', 'POST'])
+def editpatient():
+    form = formPatient()
+    # Mengambil data berdasarkan id pasien yang akan diedit
+    patient = Patients.query.get(request.form.get('id'))
+    # Apabila methodnya POST, maka aplikasi akan mengirimkan data ke database
     if request.method == 'POST':
-        member.name = form.name.data
-        member.address = form.address.data
-        member.phone = form.phone.data
-        member.email = form.email.data
-        member.updated_at = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        patient.name = form.name.data
+        patient.address = form.address.data
+        patient.phone = form.phone.data
+        patient.email = form.email.data
+        patient.complaint = form.complaint.data
+        patient.updated_at = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         db.session.commit()
-        flash("Member updated successfully!")
+        # Pesan flash ketika pasien berhasil diubah
+        flash("Patient updated successfully!")
+        # Dan akan redirect ke halaman utama
         return redirect(url_for('index'))
+    # Apabila methodnya GET, maka aplikasi akan menampilkan halaman utama
     else:
         return render_template('index.html', form=form)
 
 
-@app.route('/deletemember/<id>', methods=['GET', 'POST'])
-def deletemember(id):
-    member = Member.query.filter_by(id=id).first()
-    db.session.delete(member)
+# Route untuk menghapus data pasien
+@app.route('/deletepatient/<id>', methods=['GET', 'POST'])
+def deletepatient(id):
+    # Memilih data yang akan dihapus berdasarkan idnya
+    patient = Patients.query.filter_by(id=id).first()
+    # Menghapus data berdasarkan idnya
+    db.session.delete(patient)
     db.session.commit()
-    flash("Member deleted successfully!")
+    # Pesan flash ketika pasien berhasil dihapus
+    flash("Patient deleted successfully!")
+    # Dan akan redirect ke halaman utama
     return redirect(url_for('index'))
 
 
--- models.py
+++ models.py
@@ -1,10 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import validators, StringField, TextAreaField
+from wtforms import validators, StringField
 from wtforms.fields.html5 import TelField, EmailField
 
 
-class formMember(FlaskForm):
+# Form data pasien, menggunakan plugin WTForm
+class formPatient(FlaskForm):
     name = StringField('Name:', [validators.required()])
     address = StringField('Address:', [validators.optional()])
     phone = TelField('Phone:', [validators.required()])
     email = EmailField('Email address:', [validators.DataRequired(), validators.Email()])
+    complaint = StringField('Complaint:', [validators.required()])
